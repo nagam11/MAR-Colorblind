2fd741531c68728d3dcdbbbe51684da280b03102
Marla Narazani <marla.narazani@tum.de>
2019-01-06 05:09:45 +0200

    initial project setup

---
+++ Colorblind/Assets/Scripts/Colorblind.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+public class Colorblind : MonoBehaviour
+{
+    public enum Blindness { normal, protanopia, deuteranopia, tritanopia }
+    [Tooltip("Deuteranopia is the most common, affecting 8% of men. Protanopia is rare, affecting 2.6% of men. Tritanopia is extremely rare, affecting less than 0.1% of men. All kinds of colorblindness affect less than 1% of women.")]
+    public Blindness blindness = Blindness.normal;
+
+    Vector3 normalRed = new Vector3(1.00f, 0.00f, 0.00f);
+    Vector3 normalGreen = new Vector3(0.00f, 1.00f, 0.00f);
+    Vector3 normalBlue = new Vector3(.00f, 0.00f, 1.00f);
+    Vector3 protoRed = new Vector3(0.2f, 0.99f, -0.19f);
+    Vector3 protoGreen = new Vector3(0.16f, 0.79f, 0.04f);
+    Vector3 protoBlue = new Vector3(0.01f, -0.01f, 1.00f);
+    Vector3 deutoRed = new Vector3(0.43f, 0.72f, -0.15f);
+    Vector3 deutoGreen = new Vector3(0.34f, 0.57f, 0.09f);
+    Vector3 deutoBlue = new Vector3(-0.02f, 0.03f, 1.00f);
+    Vector3 tritoRed = new Vector3(0.97f, 0.11f, -0.08f);
+    Vector3 tritoGreen = new Vector3(0.02f, 0.82f, 0.16f);
+    Vector3 tritoBlue = new Vector3(-0.06f, 0.88f, 0.18f);
+
+    private Material material;
+
+    void Awake()
+    {
+        material = new Material(Shader.Find("Hidden/MatrixShader"));
+    }
+
+    // Postprocess the image
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Vector3 toRed = Vector3.zero;
+        Vector3 toGreen = Vector3.zero;
+        Vector3 toBlue = Vector3.zero;
+        switch (blindness)
+        {
+            case Blindness.protanopia:
+                toRed = protoRed;
+                toGreen = protoGreen;
+                toBlue = protoBlue;
+                break;
+            case Blindness.deuteranopia:
+                toRed = deutoRed;
+                toGreen = deutoGreen;
+                toBlue = deutoBlue;
+                break;
+            case Blindness.tritanopia:
+                toRed = tritoRed;
+                toGreen = tritoGreen;
+                toBlue = tritoBlue;
+                break;
+            default:
+                toRed = normalRed;
+                toGreen = normalGreen;
+                toBlue = normalBlue;
+                break;    
+        }
+
+        material.SetFloat("_rr", toRed.x);
+        material.SetFloat("_gr", toRed.y);
+        material.SetFloat("_br", toRed.z);
+        material.SetFloat("_rg", toGreen.x);
+        material.SetFloat("_gg", toGreen.y);
+        material.SetFloat("_bg", toGreen.z);
+        material.SetFloat("_rb", toBlue.x);
+        material.SetFloat("_gb", toBlue.y);
+        material.SetFloat("_bb", toBlue.z);
+        Graphics.Blit(source, destination, material);
+    }
+
+    public void NextMode()
+    {
+        var length = System.Enum.GetNames(typeof(Blindness)).Length;
+        blindness = (Blindness)(
+            ((int)blindness + 1) % length
+        );
+    }
+}
---
+++ Colorblind/Assets/Shaders/MatrixShader.shader
@@ -0,0 +1,47 @@
+﻿Shader "Hidden/MatrixShader" {
+	Properties{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+		_rg("Red -> Green", Range(-1, 1)) = 0
+		_rb("Red -> Blue", Range(-1, 1)) = 0
+		_gr("Green -> Red", Range(-1, 1)) = 0
+		_gg("Green -> Green", Range(-1, 1)) = 0
+		_gb("Green -> Blue", Range(-1, 1)) = 0
+		_br("Blue -> Red", Range(-1, 1)) = 0
+		_bg("Blue -> Green", Range(-1, 1)) = 0
+		_bb("Blue -> Blue", Range(-1, 1)) = 0
+	}
+		SubShader{
+		Pass{
+		CGPROGRAM
+#pragma vertex vert_img
+#pragma fragment frag
+
+#include "UnityCG.cginc"
+
+	uniform sampler2D _MainTex;
+	uniform float _rr;
+	uniform float _gr;
+	uniform float _br;
+	uniform float _rg;
+	uniform float _gg;
+	uniform float _bg;
+	uniform float _rb;
+	uniform float _gb;
+	uniform float _bb;
+
+
+	float4 frag(v2f_img i) : COLOR{
+		float4 c = tex2D(_MainTex, i.uv);
+		float r = c.r * _rr + c.g * _gr + c.b * _br;
+		float g = c.r * _rg + c.g * _gg + c.b * _bg;
+		float b = c.r * _rb + c.g * _gb + c.b * _bb;
+		float3 shaded = float3(r, g, b);
+
+		float4 result = c;
+		result.rgb = shaded;
+		return result;
+	}
+		ENDCG
+	}
+	}
+}
